5aca8f7857f8363018146cfc54019b7cae7726f0
ajlaaaaaa <nurajlaanadirah1302@gmail.com>
2025-06-16 23:15:40 +0800

    Add project files.

---
+++ Project_LendNRead/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Project_LendNRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.genreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.romanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thrillerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.businessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scienceFictionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.religionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mysteryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkOverdueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.genreToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.checkOverdueToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(966, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // genreToolStripMenuItem
+            // 
+            this.genreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.romanceToolStripMenuItem,
+            this.thrillerToolStripMenuItem,
+            this.businessToolStripMenuItem,
+            this.scienceFictionToolStripMenuItem,
+            this.religionToolStripMenuItem,
+            this.mysteryToolStripMenuItem});
+            this.genreToolStripMenuItem.Name = "genreToolStripMenuItem";
+            this.genreToolStripMenuItem.Size = new System.Drawing.Size(74, 29);
+            this.genreToolStripMenuItem.Text = "Genre";
+            // 
+            // romanceToolStripMenuItem
+            // 
+            this.romanceToolStripMenuItem.Name = "romanceToolStripMenuItem";
+            this.romanceToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.romanceToolStripMenuItem.Text = "Romance";
+            // 
+            // thrillerToolStripMenuItem
+            // 
+            this.thrillerToolStripMenuItem.Name = "thrillerToolStripMenuItem";
+            this.thrillerToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.thrillerToolStripMenuItem.Text = "Thriller";
+            // 
+            // businessToolStripMenuItem
+            // 
+            this.businessToolStripMenuItem.Name = "businessToolStripMenuItem";
+            this.businessToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.businessToolStripMenuItem.Text = "Business";
+            // 
+            // scienceFictionToolStripMenuItem
+            // 
+            this.scienceFictionToolStripMenuItem.Name = "scienceFictionToolStripMenuItem";
+            this.scienceFictionToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.scienceFictionToolStripMenuItem.Text = "Science Fiction";
+            // 
+            // religionToolStripMenuItem
+            // 
+            this.religionToolStripMenuItem.Name = "religionToolStripMenuItem";
+            this.religionToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.religionToolStripMenuItem.Text = "Religion";
+            // 
+            // mysteryToolStripMenuItem
+            // 
+            this.mysteryToolStripMenuItem.Name = "mysteryToolStripMenuItem";
+            this.mysteryToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.mysteryToolStripMenuItem.Text = "Mystery";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // checkOverdueToolStripMenuItem
+            // 
+            this.checkOverdueToolStripMenuItem.Name = "checkOverdueToolStripMenuItem";
+            this.checkOverdueToolStripMenuItem.Size = new System.Drawing.Size(148, 29);
+            this.checkOverdueToolStripMenuItem.Text = "Check Overdue";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(712, 77);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to Lend N Read";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(525, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(525, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password :";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(682, 313);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(245, 26);
+            this.textBoxName.TabIndex = 5;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(682, 364);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(245, 26);
+            this.textBoxPassword.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Project_LendNRead.Properties.Resources.books;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 231);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(447, 259);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 577);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem genreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem romanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thrillerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem businessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scienceFictionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem religionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mysteryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkOverdueToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Project_LendNRead/FormReceipt.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Project_LendNRead
+{
+    partial class FormReceipt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.dateTimePickerRentalDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerReturnDate = new System.Windows.Forms.DateTimePicker();
+            this.groupBoxPayment = new System.Windows.Forms.GroupBox();
+            this.radioButtonQR = new System.Windows.Forms.RadioButton();
+            this.radioButtonOnline = new System.Windows.Forms.RadioButton();
+            this.radioButtonCash = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxReceipt = new System.Windows.Forms.ListBox();
+            this.groupBoxPayment.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(421, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(421, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rental Date :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(421, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Return Date :";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(625, 95);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(314, 26);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // dateTimePickerRentalDate
+            // 
+            this.dateTimePickerRentalDate.Location = new System.Drawing.Point(625, 150);
+            this.dateTimePickerRentalDate.Name = "dateTimePickerRentalDate";
+            this.dateTimePickerRentalDate.Size = new System.Drawing.Size(314, 26);
+            this.dateTimePickerRentalDate.TabIndex = 4;
+            // 
+            // dateTimePickerReturnDate
+            // 
+            this.dateTimePickerReturnDate.Location = new System.Drawing.Point(625, 209);
+            this.dateTimePickerReturnDate.Name = "dateTimePickerReturnDate";
+            this.dateTimePickerReturnDate.Size = new System.Drawing.Size(314, 26);
+            this.dateTimePickerReturnDate.TabIndex = 5;
+            // 
+            // groupBoxPayment
+            // 
+            this.groupBoxPayment.Controls.Add(this.radioButtonCash);
+            this.groupBoxPayment.Controls.Add(this.radioButtonOnline);
+            this.groupBoxPayment.Controls.Add(this.radioButtonQR);
+            this.groupBoxPayment.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxPayment.Location = new System.Drawing.Point(426, 270);
+            this.groupBoxPayment.Name = "groupBoxPayment";
+            this.groupBoxPayment.Size = new System.Drawing.Size(278, 150);
+            this.groupBoxPayment.TabIndex = 6;
+            this.groupBoxPayment.TabStop = false;
+            this.groupBoxPayment.Text = "Payment Type :";
+            // 
+            // radioButtonQR
+            // 
+            this.radioButtonQR.AutoSize = true;
+            this.radioButtonQR.Location = new System.Drawing.Point(33, 43);
+            this.radioButtonQR.Name = "radioButtonQR";
+            this.radioButtonQR.Size = new System.Drawing.Size(74, 33);
+            this.radioButtonQR.TabIndex = 0;
+            this.radioButtonQR.TabStop = true;
+            this.radioButtonQR.Text = "QR";
+            this.radioButtonQR.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOnline
+            // 
+            this.radioButtonOnline.AutoSize = true;
+            this.radioButtonOnline.Location = new System.Drawing.Point(33, 73);
+            this.radioButtonOnline.Name = "radioButtonOnline";
+            this.radioButtonOnline.Size = new System.Drawing.Size(216, 33);
+            this.radioButtonOnline.TabIndex = 1;
+            this.radioButtonOnline.TabStop = true;
+            this.radioButtonOnline.Text = "Online Banking";
+            this.radioButtonOnline.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCash
+            // 
+            this.radioButtonCash.AutoSize = true;
+            this.radioButtonCash.Location = new System.Drawing.Point(33, 103);
+            this.radioButtonCash.Name = "radioButtonCash";
+            this.radioButtonCash.Size = new System.Drawing.Size(97, 33);
+            this.radioButtonCash.TabIndex = 2;
+            this.radioButtonCash.TabStop = true;
+            this.radioButtonCash.Text = "Cash";
+            this.radioButtonCash.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(872, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "days";
+            // 
+            // listBoxReceipt
+            // 
+            this.listBoxReceipt.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxReceipt.FormattingEnabled = true;
+            this.listBoxReceipt.ItemHeight = 29;
+            this.listBoxReceipt.Location = new System.Drawing.Point(426, 446);
+            this.listBoxReceipt.Name = "listBoxReceipt";
+            this.listBoxReceipt.Size = new System.Drawing.Size(548, 149);
+            this.listBoxReceipt.TabIndex = 8;
+            // 
+            // FormReceipt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 649);
+            this.Controls.Add(this.listBoxReceipt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBoxPayment);
+            this.Controls.Add(this.dateTimePickerReturnDate);
+            this.Controls.Add(this.dateTimePickerRentalDate);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormReceipt";
+            this.Text = "FormReceipt";
+            this.groupBoxPayment.ResumeLayout(false);
+            this.groupBoxPayment.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.DateTimePicker dateTimePickerRentalDate;
+        private System.Windows.Forms.DateTimePicker dateTimePickerReturnDate;
+        private System.Windows.Forms.GroupBox groupBoxPayment;
+        private System.Windows.Forms.RadioButton radioButtonCash;
+        private System.Windows.Forms.RadioButton radioButtonOnline;
+        private System.Windows.Forms.RadioButton radioButtonQR;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBoxReceipt;
+    }
+}
